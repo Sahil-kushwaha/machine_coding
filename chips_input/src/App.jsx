4700bf65c3b244d62649321a2b118595c33e607d
sahil kushwaha <sahilkushwaha5510@gmail.com>
2025-05-05 02:14:38 +0530

    easy:implement chips_input

---
+++ chips_input/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChipsInput from "./components/ChipsInput";
+
+
+export default function App() {
+  return (
+     
+     <ChipsInput />
+  )
+}
---
+++ chips_input/src/components/ChipsInput.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+function ChipsInput() {
+  const [chips, setChips] = useState([])
+  const [chipsInput ,setChipsInput] =useState('')
+
+  const handleChips = (e,value) => { 
+    const temp = value.trim()
+    if(e.key=="Enter" && temp ){
+      setChips((prev) => {
+      return [...prev, temp]
+     })
+    }
+   
+  }
+
+  const handleDelete = (item) => {
+    setChips((prev) => {
+      const temp = prev.filter((value)=>value!=item)
+      return temp;
+    })
+  }
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", margin: "40px 0" }}>
+      <h2>Chips Input</h2>
+      <input
+        type="text"
+        placeholder="Type a chip and press tag"
+        style={{ padding: "8px", width: "200px" }}
+        value={chipsInput}
+        onChange={(e) => { setChipsInput(e.target.value) }}
+        onKeyDown={(e)=>{handleChips(e,chipsInput)}}
+        onFocus={()=>setChipsInput("")}
+      />
+      <div className = "chips-contianer">
+        {chips.map((item,index) => (
+
+          <span key={index}>
+            {item}
+            <span
+              role="button"
+              className="del-btn"
+              onClick={() => handleDelete(item)}
+            > X
+            </span>
+          </span>
+          
+        ))
+       } 
+      </div>
+    </div>
+  );
+}
+
+export default ChipsInput;
